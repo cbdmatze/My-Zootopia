d84b39d4bfb3a1558113019e99a5fbf74eccd914
cbdmatze <mrosanowitsch@live.de>
2024-10-24 09:58:33 +0200

    access data and store

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,1 +1,12 @@
+import json
 
+def load_data(file_path):
+    """Loads a JSON file"""
+    with open(file_path, "r") as f:
+        data = json.load(f)  
+        return data
+
+file_path = 'animals_data.json'
+data = load_data(file_path)  
+
+print(data)
